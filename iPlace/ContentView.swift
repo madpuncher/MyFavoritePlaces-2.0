b2cc55e81c72c58534d15c36710efd84c53335c6
Eldar Tengizov <75984018+madpuncher@users.noreply.github.com>
2021-07-28 12:27:26 +0300

    Initial Commit

---
+++ iPlace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iPlace
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 28.07.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iPlace/iPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPlaceApp.swift
+//  iPlace
+//
+//  Created by Eʟᴅᴀʀ Tᴇɴɢɪᴢᴏᴠ on 28.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct iPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
